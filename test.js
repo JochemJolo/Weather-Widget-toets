c20d17848b2bdc09d67c6f030d666acd6b774659
Jochem <lootsmajochem@gmail.com>
2026-09-15 12:21:41 +0200

    cleaning

--- test.js
+++
@@ -1,8 +0,0 @@
-fetch("/weather")
-    .then(response => response.json())
-    .then(data => {
-        console.log("City:", data.city);
-        console.log("Temperature:", data.temperature + " °C");
-        console.log("Weather:", data.weather);
-        console.log("Time:", data.time);
-    });
